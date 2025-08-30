87a05f8ad2a1c89d3a58d63dbd832a40879b647b
Stabats <89476581+Stabats@users.noreply.github.com>
2025-08-30 19:48:27 -0400

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -38,17 +38,18 @@ charadex.sheet = {
     prompts:       "prompts",
     faq:           "faq",
     staff:         "mods",
+    seekers:       "seekers",
   },
 
   options: {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
-    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
+    statuses: ['All', 'Active', 'Haitus', 'Inactive'],
     rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
-    itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
-
+    species: ['All', 'PC', 'NPC'],
+    itemTypes: ['All', 'Currency', 'Ingridents', 'Plots', 'Homes', 'Decor', 'Misc'],
+    traitTypes: ['All', 'Aether', 'Air', 'Earth', 'Fire', 'Water'],
+    level: ['All', '1', '2', '3', '4', '5', '6'],
   }
 
 }
