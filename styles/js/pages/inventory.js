6b466ade0dfbcf892eeb5df841fb8ff4ac0587c8
Stabats <89476581+Stabats@users.noreply.github.com>
2025-08-30 12:15:24 -0400

    Update inventory.js

--- styles/js/pages/inventory.js
+++ styles/js/pages/inventory.js
@@ -35,13 +35,6 @@ document.addEventListener("DOMContentLoaded", async () => {
           );
         }
 
-        if (charadex.tools.checkArray(profile.seekers)) {
-          let designs = await charadex.initialize.page(
-            profile.seekers,
-            charadex.page.inventory.relatedData['seekers'],
-          );
-        }
-
         // Logs
         if (charadex.tools.checkArray(profile.inventorylog)) {
           let logs = await charadex.initialize.page(
