03589c4096d4a0e390fc66d9c612520f8f150bbe
Stabats <89476581+Stabats@users.noreply.github.com>
2025-12-14 21:28:50 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -47,7 +47,7 @@ charadex.sheet = {
     statuses: ['All', 'Active', 'Haitus', 'Inactive'],
     rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
     species: ['All', 'PC', 'NPC'],
-    itemTypes: ['All', 'Currency', 'Ingridents', 'Plots', 'Homes', 'Decor', 'Misc'],
+    itemTypes: ['All', 'Currency', 'Ingredents', 'Plots', 'Homes', 'Decor', 'Misc'],
     elementTypes: ['All', 'Aether', 'Air', 'Earth', 'Fire', 'Water'],
     level: ['All', '1', '2', '3', '4', '5', '6'],
   }
