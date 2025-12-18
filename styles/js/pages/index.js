30d0822d7f840d0c15e02cd9b23141efd463e2a7
Stabats <89476581+Stabats@users.noreply.github.com>
2025-12-18 01:39:47 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -67,10 +67,20 @@ document.addEventListener("DOMContentLoaded", async () => {
   let badges = await charadex.initialize.page(null, charadex.page.index.badges, (arr) => {
     
     // Splice the silly little array
-    let sliceAmount = charadex.page.index.badges.amount || 6;
-    arr.splice(sliceAmount, arr.length);
+      let sliceAmount = charadex.page.index.badges.amount || 4;
+      arr.splice(sliceAmount, arr.length);
 
-  });
+    }, (data) => {
+
+      // Add the silly little prompt stuff here too
+      $('.cd-prompt-background').each(function(i) {
+        const element = $(this);
+        const image = data.array[i]?.image;
+        element.attr('style', `background-image: url(${image})`);
+      });
+      
+    }
+  };
 
 
   /* Load Page
